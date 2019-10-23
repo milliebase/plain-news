7d1616e83b14ca004b2dc1f909ac0ba576c7dd57
milliebase <betsyalva@milliebase.se>
2019-10-23 13:10:19 +0200

    changed name of key in articles

--- data.php
+++ data.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 // This is the file where you can keep your data arrays such as articles and
 // authors.
 
-$authors = [
+$authors =
+[
     [
         'id' => 1,
         'name' => 'Jon Snow',
@@ -28,13 +29,14 @@ $authors = [
     ],
 ];
 
-$articles = [
+$articles =
+[
     [
         'title' => 'I do really know nothing',
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br>
                         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br>
                         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        'author_id' => 1,
+        'authorId' => 1,
         'published' => '2019-10-23',
         'likes' => 2345,
     ],
@@ -43,7 +45,7 @@ $articles = [
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br>
                         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br>
                         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        'author_id' => 5,
+        'authorId' => 5,
         'published' => '2019-09-04',
         'likes' => 148796,
     ],
@@ -52,7 +54,7 @@ $articles = [
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br>
                         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br>
                         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        'author_id' => 4,
+        'authorId' => 4,
         'published' => '2019-10-23',
         'likes' => 46357,
     ],
@@ -61,7 +63,7 @@ $articles = [
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br>
                         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br>
                         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        'author_id' => 3,
+        'authorId' => 3,
         'published' => '2018-11-03',
         'likes' => 87044,
     ],
@@ -70,7 +72,7 @@ $articles = [
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br>
                         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br>
                         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        'author_id' => 2,
+        'authorId' => 2,
         'published' => '2019-08-14',
         'likes' => 487,
     ],
