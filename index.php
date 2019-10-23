d6686a98a6ec92a0f97eb5d81c12efa5bb5c6837
milliebase <betsyalva@milliebase.se>
2019-10-23 10:07:55 +0200

    Add .gitignore and php-files.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// This is the file where you can keep your HTML markup. We should always try to
+// keep us much logic out of the HTML as possible. Put the PHP logic in the top
+// of the files containing HTML or even better; in another PHP file altogether.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Newsfeed</title>
+</head>
+<body>
+    
+</body>
+</html>
