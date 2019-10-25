e0bc5653cc84101c12bc5dfd29c54ad2b5c553d6
milliebase <betsyalva@milliebase.se>
2019-10-25 22:12:21 +0200

    Update data.php

--- data.php
+++ data.php
@@ -7,11 +7,36 @@ declare(strict_types=1);
 
 $authors =
 [
-    1 => ['name' => 'Jon Snow',],
-    2 => ['name' => 'Sansa Stark',],
-    3 => ['name' => 'Arya Stark',],
-    4 => ['name' => 'Brianne of Tarth',],
-    5 => ['name' => 'Tyrion Lannister',],
+    1    => [
+        'firstName' => 'Jon',
+        'lastName' => 'Snow',
+        'portrait' => '/img/jonsnow.jpg',
+        'class' => 'jon',
+    ],
+    2 => [
+        'firstName' => 'Sansa',
+        'lastName' => 'Stark',
+        'portrait' => '/img/sansastark.png',
+        'class' => 'sansa',
+    ],
+    3 => [
+        'firstName' => 'Arya',
+        'lastName' => 'Stark',
+        'portrait' => '/img/aryastark.jpg',
+        'class' => 'arya',
+    ],
+    4 => [
+        'firstName' => 'Brienne',
+        'lastName' => 'of Tarth',
+        'portrait' => '/img/brienneoftarth.jpg',
+        'class' => 'brienne',
+    ],
+    5 => [
+        'firstName' => 'Tyrion',
+        'lastName' => 'Lannister',
+        'portrait' => '/img/tyrionlannister.jpg',
+        'class' => 'tyrion',
+    ],
 ];
 
 $articles =
@@ -68,7 +93,7 @@ $articles =
 
         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
         'authorId' => 2,
-        'published' => '2019-08-14',
+        'published' => '2017-08-14',
         'likes' => 487,
     ],
     [
@@ -102,7 +127,7 @@ $articles =
         Mauris eleifend sem sed nunc congue feugiat. Vivamus vitae augue eget odio dapibus laoreet sit amet a arcu. Aenean et consequat sem. Aliquam hendrerit facilisis purus id posuere. Ut at nisi mi. Cras arcu nisl, imperdiet ut felis id, tristique luctus libero. Nam mattis semper luctus. Quisque molestie lacinia tortor malesuada dictum. Vestibulum suscipit augue neque, eget tempus tortor sodales ut. Donec scelerisque massa id volutpat posuere. Sed vestibulum lacus non fringilla mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
         'authorId' => 4,
         'published' => '2019-12-14',
-        'likes' => 487,
+        'likes' => 2356,
     ],
     [
         'title' => 'Queen of the North',
@@ -116,7 +141,7 @@ $articles =
         'likes' => 1347,
     ],
     [
-        'title' => 'Tales of an imp.',
+        'title' => 'Tales of an imp',
         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem a nisl ultricies sollicitudin eu quis ligula. Quisque libero tellus, aliquet at lacinia at, volutpat et purus. Cras cursus augue et lorem sollicitudin cursus. Vestibulum non nibh faucibus metus vulputate tincidunt non id felis. Fusce finibus cursus nibh et pharetra. Fusce lectus nisi, malesuada eu libero quis, pellentesque scelerisque tortor. Sed elementum cursus arcu, eget venenatis felis iaculis nec. Pellentesque elit quam, auctor ut metus at, lobortis dictum augue. Nam consequat nibh nec rutrum finibus.<br><br>
 
         Etiam aliquam erat a ipsum lobortis luctus. Nam ac hendrerit quam, eget venenatis sem. Curabitur at mauris pellentesque, bibendum justo vel, pellentesque augue. Donec cursus eget nulla vel pellentesque. Donec blandit sapien arcu, a auctor nibh volutpat mattis. Sed eget volutpat leo, finibus condimentum eros. Morbi finibus eros nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse euismod nulla eu purus vestibulum tincidunt. Donec semper lobortis malesuada. Ut convallis metus eros, et fermentum felis rhoncus vel. Fusce a viverra nisi. Praesent mollis egestas tortor, a tincidunt sapien.<br><br>
