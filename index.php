b248b190c1bcdc30774295c2ae5a8adc34e73a28
milliebase <betsyalva@milliebase.se>
2019-10-25 22:11:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,46 @@
 <?php
-// This is the file where you can keep your HTML markup. We should always try to
-// keep us much logic out of the HTML as possible. Put the PHP logic in the top
-// of the files containing HTML or even better; in another PHP file altogether.
 
-require __DIR__.'/data.php';
-require __DIR__.'/functions.php';
-
-usort($articles, "sortDates"); //Sort the dates of the articles.
+require __DIR__.'/header.php';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Newsfeed</title>
-    <link href='https://unpkg.com/sanitize.css' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <?php foreach ($articles as $i => $article) :?>
-        <article id="<?php echo $i; ?>">
-            <h1><?php echo $article['title']; ?></h1>
-            <h3 class="author"><?php echo $authors[$article['authorId']]['name'];?></h3>
-            <h3 class="date"><?php echo $article['published'] ?></h3>
-            <p class="content"><?php echo $article['content']; ?></p>
-            <button class="likes"><?php echo $article['likes']; ?></button>
-        </article>
-        <?php endforeach; ?>
-    </div>
-</body>
-</html>
+<section class="container">
+<?php foreach ($articles as $i => $article) :?>
+
+    <?php
+        $title = $article['title'];
+        $text = $article['content'];
+        $portrait = $authors[$article['authorId']]['portrait'];
+        $date = $article['published'];
+        $likes = $article['likes'];
+        $class = $authors[$article['authorId']]['class'];
+
+        $firstName = $authors[$article['authorId']]['firstName'];
+        $lastName = $authors[$article['authorId']]['lastName'];
+        $name = "$firstName $lastName";
+    ?>
+
+    <article id="<?php echo $i; ?>">
+
+            <div class="newsTxt">
+                <h2><?php echo $title; ?></h2>
+                <h3 class="date">Published: <?php echo $date; ?></h3>
+                <button onclick="getLikes()" class="likes"><?php echo $likes; ?></button>
+                <p class="text"><?php echo $text; ?></p>
+            </div>
+
+            <div class="info <?php echo $class ?>">
+                <h3 class="author">Written by <br><?php echo $name; ?></h3>
+                <img src="<?php echo $portrait; ?>" alt="<?php echo $name?>">
+            </div>
+
+    </article>
+
+<?php endforeach; ?>
+</section>
+
+<?php
+
+require __DIR__.'/footer.php';
+
+?>
