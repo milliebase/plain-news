307001abc9aa80148ca7e9d89980bcaf53adf2c6
milliebase <betsyalva@milliebase.se>
2019-10-28 20:53:49 +0100

    corrections on sortDates function

--- functions.php
+++ functions.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/data.php';
-
 /**
  * Callback function for usort and sort the dates in descending order.
  *
  * @param array $a
- * @param arrray $b
+ * @param array $b
  * @return integer
  */
-function sortDates (array $a, array $b): int
+function sortDates (array $a, array $b): bool
 {
-    return strtotime($b['published']) - strtotime($a['published']);
+    return strtotime($a['published']) < strtotime($b['published']);
 };
