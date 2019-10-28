3915f7dec639e3a0866a3d2fe0f1c34bf8b262a9
milliebase <betsyalva@milliebase.se>
2019-10-28 20:53:36 +0100

    added script to body

--- index.php
+++ index.php
@@ -5,39 +5,43 @@ require __DIR__.'/header.php';
 ?>
 
 <section class="container">
-<?php foreach ($articles as $i => $article) :?>
 
-    <?php
-        $title = $article['title'];
-        $text = $article['content'];
-        $portrait = $authors[$article['authorId']]['portrait'];
-        $date = $article['published'];
-        $likes = $article['likes'];
-        $class = $authors[$article['authorId']]['class'];
+    <?php foreach ($articles as $i => $article) :?>
 
-        $firstName = $authors[$article['authorId']]['firstName'];
-        $lastName = $authors[$article['authorId']]['lastName'];
-        $name = "$firstName $lastName";
-    ?>
+        <?php
+            $title = $article['title'];
+            $text = $article['content'];
+            $portrait = $authors[$article['authorId']]['portrait'];
+            $date = $article['published'];
+            $likes = $article['likes'];
+            $class = $authors[$article['authorId']]['class'];
 
-    <article id="<?php echo $i; ?>">
+            $firstName = $authors[$article['authorId']]['firstName'];
+            $lastName = $authors[$article['authorId']]['lastName'];
+            $name = "$firstName $lastName";
+        ?>
 
-            <div class="newsTxt">
-                <h2><?php echo $title; ?></h2>
-                <h3 class="date">Published: <?php echo $date; ?></h3>
-                <button onclick="getLikes()" class="likes"><?php echo $likes; ?></button>
-                <p class="text"><?php echo $text; ?></p>
-            </div>
+        <article id="<?php echo $i?>">
 
-            <div class="info <?php echo $class ?>">
-                <h3 class="author">Written by <br><?php echo $name; ?></h3>
-                <img src="<?php echo $portrait; ?>" alt="<?php echo $name?>">
-            </div>
+                <div class="newsTxt">
+                    <h2><?php echo $title; ?></h2>
+                    <h3 class="date">Published: <?php echo $date; ?></h3>
+                    <p class="text"><?php echo $text; ?></p>
+                    <button class="likeBtn <?php echo $class; ?>"><span><?php echo $likes; ?></span> likes</button>
+                </div>
 
-    </article>
+                <div class="info <?php echo $class ?>">
+                    <h3 class="author">Written by <br><?php echo $name; ?></h3>
+                    <img src="<?php echo $portrait; ?>" alt="<?php echo $name?>">
+                </div>
 
-<?php endforeach; ?>
+        </article>
+
+    <?php endforeach; ?>
 </section>
+
+
+<script src="main.js"></script>
 
 <?php
 
