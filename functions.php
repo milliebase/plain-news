c0c14c7258fa1e8a02d652f1b2f814cb9cf39a00
milliebase <betsyalva@milliebase.se>
2019-10-30 22:13:00 +0100

    added function filteredAuthor

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Callback function for usort and sort the dates in descending order.
+ * Callback function for usort and sorts the dates in descending order.
  *
  * @param array $a
  * @param array $b
@@ -12,4 +12,20 @@ declare(strict_types=1);
 function sortDates (array $a, array $b): bool
 {
     return strtotime($a['published']) < strtotime($b['published']);
+};
+
+/**
+ * Gives a new array of items by given id
+ *
+ * @param array $items
+ * @param integer $id
+ * @return array
+ */
+function filteredAuthor (array $items, int $id): array
+{
+        $items = array_filter($items, function($item) use ($id) {
+            return $item['authorId'] === $id ;
+        });
+
+        return $items;
 };
